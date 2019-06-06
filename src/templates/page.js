9466a25e3b2ba63c4fe5e0683cd83e697ab4caef
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-06-06 09:53:08 -0500

    Add templates components

---
+++ src/templates/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Page = ({ pageContext }) => {
+  const { title, content } = pageContext
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{__html: content}} />
+    </Layout>
+  )
+}
+
+export default Page
